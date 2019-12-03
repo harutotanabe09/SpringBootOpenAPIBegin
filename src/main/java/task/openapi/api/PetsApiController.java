74837f9d47f44154efda6de48f6e892ad091e7d7
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-12-03 21:37:45 +0900

    method override

--- src/main/java/task/openapi/api/PetsApiController.java
+++ src/main/java/task/openapi/api/PetsApiController.java
@@ -1,6 +1,7 @@
 package task.openapi.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,9 +10,25 @@ import org.openapi.example.model.Pet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @RestController
 public class PetsApiController implements PetsApi{
+
+	@Override
+	public Optional<NativeWebRequest> getRequest() {
+		return PetsApi.super.getRequest();
+	}
+
+	@Override
+	public ResponseEntity<Void> createPets() {
+		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	}
+
+	@Override
+	public ResponseEntity<Pet> showPetById(String petId) {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 	@Override
 	public ResponseEntity<List<Pet>> listPets(@Valid Integer limit) {
